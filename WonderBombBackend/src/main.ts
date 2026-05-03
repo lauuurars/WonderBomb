e70c8406bebc31c703f39adf4c801551b53e3a33
lauuurars <1120098177@u.icesi.edu.co>
2026-05-02 22:25:02 -0500

    feat: requirement 3 complete, configured sockets!

--- WonderBombBackend/src/main.ts
+++ WonderBombBackend/src/main.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express"
+import socketio from "socket.io"
 import { createServer } from "http"
 import cors from "cors"
 import "dotenv/config"
 
 import { AuthRouter } from "./routes/auth/auth.router"
 import { GameRouter } from "./routes/game/game.router"
+import { addPlayer, getState, growBall, removePlayer } from './game/game';
 
 const app = express()
 app.use(cors())
@@ -23,3 +25,43 @@ rawServer.listen(8080, () => {
     console.log("Server running on port 8080")
 })
 
+const io = new socketio.Server(rawServer, {
+    path: "/real-time",
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("connected:", socket.id)
+
+    addPlayer(socket.id) // registramos nuevo jugador 
+
+    // avisamos a todos los clientes el estado completo del juego
+    io.emit("game-update", {
+        players: getState()
+    })
+
+    // evento shake 
+    socket.on("shake", (payload: { magnitude: number }) => {
+
+        const magnitude = Number(payload?.magnitude ?? 0)
+        const exploded = growBall(socket.id, magnitude)
+
+        io.emit("game-update", { players: getState() }) // actualiza el estado nuevo
+
+        // emitimos q el juego terminó y enviamos el ganador
+        if (exploded) {
+            io.emit("game-over", {
+                winner: socket.id
+            })
+        }
+    })
+
+    // eliminamos jugador y actualizamos el estado 
+    socket.on("disconnect", () => {
+        console.log("disconnected:", socket.id)
+        removePlayer(socket.id)
+        io.emit("game-update", { players: getState() })
+    })
+})
